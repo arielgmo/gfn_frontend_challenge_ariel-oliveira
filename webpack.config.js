5c2ba08dff0a894e30c20556deb85a95e742814e
Ariel G M de Oliveira <ariel.oliveira@vistajet.com>
2018-10-10 14:47:02 -0300

    Initial commit webpack, eslint, resources

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
